eda535700ad42e6bce11e50fde5babafa3a8acdc
portalchu <giry8647@gmail.com>
2024-11-05 18:21:36 +0900

    test

--- mysite/django3.sh
+++ mysite/django3.sh
@@ -5,5 +5,8 @@ sleep 5s
 echo "python3 migrate"
 python3 mysite/manage.py migrate
 sleep 10s
+echo "python3 collectstatic"
+python3 mysite/manage.py collectstatic
+sleep 5s
 echo "python3 runserver 0.0.0.0:8000"
 python3 mysite/manage.py runserver 0.0.0.0:8000
